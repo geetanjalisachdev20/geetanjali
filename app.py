506b72242caabcbd10fcb0c767c0dfe9eba9858a
geetanjalisachdev20 <46290958+geetanjalisachdev20@users.noreply.github.com>
2022-05-13 23:09:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ disease_model.eval()
 
 # Loading crop recommendation model
 
-crop_recommendation_model_path = 'models/RandomForest.pkl'
-crop_recommendation_model = pickle.load(
+#crop_recommendation_model_path = 'models/RandomForest.pkl'
+#crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
 
