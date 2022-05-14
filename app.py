72dd59db3d5927c92b26ddfc4ddcaf3fd29a397e
geetanjalisachdev20 <46290958+geetanjalisachdev20@users.noreply.github.com>
2022-05-14 13:05:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,9 +67,9 @@ disease_model.eval()
 
 # Loading crop recommendation model
 
-#crop_recommendation_model_path = 'models/RandomForest.pkl'
-#crop_recommendation_model = pickle.load(
-  #open(crop_recommendation_model_path, 'rb'))
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
 
 
 # =========================================================================================
@@ -133,10 +133,25 @@ app = Flask(__name__)
 
 @ app.route('/')
 def home():
-    title = 'Home'
+    title = 'Harvestify - Home'
     return render_template('index.html', title=title)
 
+# render crop recommendation form page
 
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Harvestify - Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# render fertilizer recommendation form page
+
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Harvestify - Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
 
 # render disease prediction input page
 
@@ -147,14 +162,85 @@ def home():
 
 # RENDER PREDICTION PAGES
 
+# render crop recommendation result page
 
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+# render fertilizer recommendation result page
+
+
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'Harvestify - Fertilizer Suggestion'
+
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    # ph = float(request.form['ph'])
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer-result.html', recommendation=response, title=title)
 
 # render disease prediction result page
 
 
-@ app.route('/disease-predict', methods=['GET', 'POST'])
+@app.route('/disease-predict', methods=['GET', 'POST'])
 def disease_prediction():
-    title = 'Disease Detection'
+    title = 'Harvestify - Disease Detection'
 
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -175,3 +261,5 @@ def disease_prediction():
 
 
 # ===============================================================================================
+if __name__ == '__main__':
+    app.run(debug=False)
