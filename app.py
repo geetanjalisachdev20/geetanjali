db5df687c6c07425b97de5c1623b0e241bc161e5
geetanjalisachdev20 <46290958+geetanjalisachdev20@users.noreply.github.com>
2022-05-14 16:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,30 +71,7 @@ disease_model.eval()
 
 # =========================================================================================
 
-# Custom functions for calculations
 
-
-def weather_fetch(city_name):
-    """
-    Fetch and returns the temperature and humidity of a city
-    :params: city_name
-    :return: temperature, humidity
-    """
-    api_key = config.weather_api_key
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    response = requests.get(complete_url)
-    x = response.json()
-
-    if x["cod"] != "404":
-        y = x["main"]
-
-        temperature = round((y["temp"] - 273.15), 2)
-        humidity = y["humidity"]
-        return temperature, humidity
-    else:
-        return None
 
 
 def predict_image(img, model=disease_model):
